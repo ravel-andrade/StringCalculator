686aca28d3964369e2f7f7348168bbcfdedeb9e4
Ravel Andrade <ravel.gou@gmail.com>
2021-04-01 14:29:28 -0300

    testAddWithString refactored

--- src/test/java/TestStringCalculator.java
+++ src/test/java/TestStringCalculator.java
@@ -2,9 +2,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestStringCalculator {
+
     @Test
     public void testAddWithString(){
         String value = "a";
         Assertions.assertEquals(Calculator.add(value), "a");
     }
+
+
 }
